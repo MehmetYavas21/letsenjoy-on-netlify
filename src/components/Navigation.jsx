07478f14984baac500a9cb3d4d441c99d87138a9
Mehmet Yavas <fzt.mehmetyavas@gmail.com>
2023-09-04 12:50:41 +0200

    some changes on navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 export const Navigation = () => {
   return (
     <Flex
-      bg="blackAlpha.700"
-      color="whiteAlpha.700"
+      bg="blue.700"
+      color="#fff"
       p={4}
       alignItems="center"
       justifyContent="space-between"
@@ -14,17 +14,16 @@ export const Navigation = () => {
       <Heading
         as="h1"
         size="lg"
-        color="whatsapp.100"
-        background="green.800"
+        color="blue.400"
         p="15px"
-        borderRadius="50px"
-        fontFamily="fantasy"
+        fontFamily="fantasy, sans-serif"
+        textDecoration="none"
         _hover={{
           size: "xl",
-          transition: "bg",
-          bg: "green.200",
+          bg: "blue.100",
           color: "white",
           textDecoration: "none",
+          transition: "all, .5s, ease",
         }}
       >
         <Link to="/"> EnJoY</Link>
